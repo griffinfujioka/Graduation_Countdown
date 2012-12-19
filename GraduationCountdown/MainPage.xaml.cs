0823d3a5168b251cec3b6bb5fd1e79f36fdb45f6
Griffin Fujioka <fujiokag@hotmail.com>
2012-12-18 19:31:45 -0800

    MVP complete!
    Had a major code session today and achieved a lot of the major
    functionality. Upon installing the app users will be prompted for their
    graduation date which will be saved locally. I added a top app bar and a
    button on there for users to change their graduation date. The countdown
    code was the same as was used in my Christmas Countdown so that was easy
    to complete.
    
    TODO:
    - Artwork
    - Live tile
    - Other cool settings/ customization options?

--- GraduationCountdown/MainPage.xaml.cs
+++ GraduationCountdown/MainPage.xaml.cs
@@ -11,7 +11,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.Storage;          /* for ApplicationData */ 
+using Windows.Storage;          // for ApplicationData
+using Windows.Globalization.DateTimeFormatting; // datetime formatting
+using Windows.ApplicationModel.Background;      // background tasks 
+using Clock.WinRT;
 
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
@@ -23,13 +26,23 @@ namespace GraduationCountdown
     /// </summary>
     public sealed partial class MainPage : GraduationCountdown.Common.LayoutAwarePage
     {
+        #region Variable declarations
         private Windows.Foundation.Collections.IPropertySet appSettings;
-        private const string dateKey = "dateKey"; 
+        private const string dateKey = "dateKey";
+        private const string TASKNAMEUSERPRESENT = "TileSchedulerTaskUserPresent";
+        private const string TASKNAMETIMER = "TileSchedulerTaskTimer";
+        private const string TASKENTRYPOINT = "Clock.WinRT.TileSchedulerTask";
+        public static DateTime GraduationDate = new DateTime();
+        private DispatcherTimer timer;
+        #endregion 
 
+        #region Page constructor
         public MainPage()
         {
             this.InitializeComponent();
             appSettings = ApplicationData.Current.LocalSettings.Values;
+
+            Loaded += OnLoaded; 
 
             #region Initialize all selected values in the graduation date popup
             monthComboBox.SelectedIndex = 0;
@@ -37,16 +50,114 @@ namespace GraduationCountdown
             yearComboBox.SelectedIndex = 0;
             #endregion 
 
+            #region Show the date PopUp if there isn't a saved date 
             if (!appSettings.ContainsKey(dateKey))
             {
+                untilTxtBlock.Visibility = Visibility.Collapsed;
+                countdownTxtBlock.Visibility = Visibility.Collapsed; 
                 datePopUp.IsOpen = true; 
             }
             else
             {
                 datePopUp.IsOpen = false;
+                string date = appSettings[dateKey].ToString();
+                var tempArray = date.Split(' ');    // Results in  tempArray[0] = xx/xx/xx
+                var dateArray = tempArray[0].Split('/');
+                int month = Convert.ToInt32(dateArray[0]);
+                int day = Convert.ToInt32(dateArray[1]);
+                int year = Convert.ToInt32(dateArray[2]);
+                GraduationDate = new DateTime(year, month, day); 
+            }
+            #endregion 
+        }
+        #endregion 
+
+        #region OnNavigatedTo
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            
+        }
+        #endregion 
+
+        #region OnLoaded
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            int CurrentYear = DateTime.Now.Year;                /* Get Current Year */
+            DateTime NewYear = new DateTime(DateTime.Now.Year + 1, 1, 1);
+
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+
+            timer.Tick += new EventHandler<object>(OnTick);
+
+            try
+            {
+                timer.Start(); 
+                
+            }
+            catch (Exception ex)
+            {
             }
         }
+        #endregion 
 
+        #region CreateClockTask
+        private static async void CreateClockTask()
+        {
+            var result = await BackgroundExecutionManager.RequestAccessAsync();
+            if (result == BackgroundAccessStatus.AllowedMayUseActiveRealTimeConnectivity ||
+                result == BackgroundAccessStatus.AllowedWithAlwaysOnRealTimeConnectivity)
+            {
+                ClockTileScheduler.CreateSchedule();
+
+                EnsureUserPresentTask();
+                EnsureTimerTask();
+            }
+        }
+        #endregion 
+
+        #region EnsureUserPresentTask
+        private static void EnsureUserPresentTask()
+        {
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+                if (task.Value.Name == TASKNAMEUSERPRESENT)
+                    return;
+
+            BackgroundTaskBuilder builder = new BackgroundTaskBuilder();
+            builder.Name = TASKNAMETIMER;
+            builder.TaskEntryPoint = TASKENTRYPOINT;
+            builder.SetTrigger(new SystemTrigger(SystemTriggerType.UserPresent, false));
+            builder.Register();
+        }
+        #endregion 
+
+        #region EnsureTimerTask
+        private static void EnsureTimerTask()
+        {
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+                if (task.Value.Name == TASKNAMETIMER)
+                    return;
+
+            BackgroundTaskBuilder builder = new BackgroundTaskBuilder();
+            builder.Name = TASKNAMETIMER;
+            builder.TaskEntryPoint = TASKENTRYPOINT;
+            builder.SetTrigger(new TimeTrigger(180, false));
+            builder.Register();
+        }
+        #endregion 
+
+        #region OnTick
+        private void OnTick(object sender, object e)
+        {
+            var timeLeft = GraduationDate - DateTime.Now;
+
+            countdownTxtBlock.Text = string.Format("{0:D2} days\n{1:D2} hours\n{2:D2} minutes\n{3:D2} seconds", timeLeft.Days, timeLeft.Hours, timeLeft.Minutes, timeLeft.Seconds);
+        }
+        #endregion 
+
+        #region LoadState
         /// <summary>
         /// Populates the page with content passed during navigation.  Any saved state is also
         /// provided when recreating a page from a prior session.
@@ -59,7 +170,9 @@ namespace GraduationCountdown
         protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
         {
         }
+        #endregion 
 
+        #region SaveState
         /// <summary>
         /// Preserves state associated with this page in case the application is suspended or the
         /// page is discarded from the navigation cache.  Values must conform to the serialization
@@ -69,10 +182,82 @@ namespace GraduationCountdown
         protected override void SaveState(Dictionary<String, Object> pageState)
         {
         }
+        #endregion 
 
+        #region datePopUp Save Button Clicked
         private void saveDateBtn_Click(object sender, RoutedEventArgs e)
         {
+            string monthString = monthComboBox.SelectedValue.ToString();
+            int monthInt = 0; 
+            int day = (int)dayComboBox.SelectedValue;
+            int year = (int)yearComboBox.SelectedValue;
 
+            #region switch-statement to setup month as integer value
+            switch (monthString)
+            {
+                case "January":
+                    monthInt = 1; 
+                    break; 
+                case "February":
+                    monthInt = 2;
+                    break; 
+                case "March":
+                    monthInt = 3;
+                    break; 
+                case "April":
+                    monthInt = 4;
+                    break; 
+                case "May":
+                    monthInt = 5;
+                    break; 
+                case "June":
+                    monthInt = 6;
+                    break; 
+                case "July":
+                    monthInt = 7;
+                    break; 
+                case "August":
+                    monthInt = 8;
+                    break; 
+                case "September":
+                    monthInt = 9;
+                    break; 
+                case "October":
+                    monthInt = 10;
+                    break; 
+                case "November":
+                    monthInt = 11;
+                    break; 
+                case "December":
+                    monthInt = 12;
+                    break; 
+                default: break; 
+            }
+            #endregion 
+
+            GraduationDate = new DateTime(year, monthInt, day);
+
+            appSettings[dateKey] = GraduationDate.ToString();  
+            datePopUp.IsOpen = false;   // Close the PopUp
+            untilTxtBlock.Visibility = Visibility.Visible;
+            countdownTxtBlock.Visibility = Visibility.Visible; 
         }
+        #endregion 
+
+        #region Change date button clicked
+        private void changeDateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            untilTxtBlock.Visibility = Visibility.Collapsed;
+            countdownTxtBlock.Visibility = Visibility.Collapsed; 
+            datePopUp.IsOpen = true;
+        }
+        #endregion 
+
+        #region datePopup Cancel Button Clicked
+        private void cancelBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+            datePopUp.IsOpen = false; 
+        }
+        #endregion 
     }
 }
